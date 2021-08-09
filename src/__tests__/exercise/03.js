79cd189499a135b6427572a31d1af13c3ee4a50d
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-08-08 19:36:59 -0700

    completed 03

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -2,16 +2,18 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-// 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+// 🐨 add `screen` to the import here: (X)
+import {render, fireEvent, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+  render(<Counter />)
+  // 🐨 replace these with screen queries (X)
+  // const [decrement, increment] = container.querySelectorAll('button')
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  const increment = screen.getByRole('button', {name: /increment/i})
+  const message = screen.getByText(/current count/i)
+
 
   expect(message).toHaveTextContent('Current count: 0')
   fireEvent.click(increment)
@@ -19,3 +21,20 @@ test('counter increments and decrements when the buttons are clicked', () => {
   fireEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
+
+
+
+// Screen Utility /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// We're basically refactoring our test to make it more resilient to changes that the code base may experience over 
+// time. Now, if we add another button, it doesn't really matter because as long as the user is still able to find the 
+// decrement button by its name and the fact that it's actually a button, our test will as well.
+
+// The user is going to find this message by seeing the text "Current count,” then a number. We can do the same thing
+// here by saying, our message is screen.getByText, and we'll say current count.
+
+// We're going to get the element that has the text "Current count." That's going to be our message element. Then we 
+// no longer need that container at all and we can just say, "Hey, render this to the page," then my screen will be 
+// responsible for grabbing things on the page.
+
+// Broswer Interactions /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
