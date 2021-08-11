cc47b6f1e106b2dedc1aa52bcac4a23c9f0b32b5
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-08-11 12:10:39 -0700

    completed 07

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -7,16 +7,25 @@ import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+  // 🐨 update the `render` call above to use the wrapper option using the (X)
+  // ThemeProvider (X)
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="light">{children}</ThemeProvider>
+  )
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
 })
 
 /* eslint no-unused-vars:0 */
+
+
+// Wrapper Component //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// In review, all that we did here was we rendered the component we wanted. We noticed that it wasn't getting all of 
+// the context values that it needed. We use the wrapper option and created a wrapper component that wrapped our 
+// children in our context providers. That ensured that our consumer of that context had access to the context value 
+// provided to us from the context provider.
